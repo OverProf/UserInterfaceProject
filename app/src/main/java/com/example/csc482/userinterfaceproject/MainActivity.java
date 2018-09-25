cf77cff120e4c0fb8c9725767d4f2376664f0431
Scott Graham <greylander@gmail.com>
2018-09-24 21:48:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/csc482/userinterfaceproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.csc482.userinterfaceproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
